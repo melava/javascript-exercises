1f4f62c1349772140867061b89efeac8f9e24cdf
Melanie Vanput <melanie.vanput@gmail.com>
2020-12-03 19:10:23 +0100

    resolve repeat string

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+let string;
+let iteration;
 
+const repeatString = function(string, iteration) {
+    let newString = '';
+    if (iteration < 0) {
+        newString = 'ERROR';
+    }
+    for (let i = 0; i < iteration; i++) {
+        newString += string;
+    }
+    return newString
 }
 
 module.exports = repeatString
