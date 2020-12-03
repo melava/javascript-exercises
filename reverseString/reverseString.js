fd6741606d60125ee3106ee5086d6f60b847b09f
Melanie Vanput <melanie.vanput@gmail.com>
2020-12-03 19:11:30 +0100

    resolve reverse string exercice

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let splittedString = string.split('');
+  let arrayLength = splittedString.length;
+  let reversedString = '';
+  for (let i = arrayLength - 1; i >= 0; i--) {
+      reversedString += splittedString[i];
+  }
+  return reversedString
 }
 
 module.exports = reverseString
