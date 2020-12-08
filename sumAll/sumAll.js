bca6ef226077f2b07ca651723f6d44931940f818
Melanie Vanput <melanie.vanput@gmail.com>
2020-12-08 15:40:06 +0100

    resolve sum all exercice

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(initNumber, endNumber) {
+        let sum = 0;
+        if (typeof initNumber != "number" || typeof endNumber != "number"|| initNumber < 0 || endNumber < 0) {
+            return 'ERROR';
+        } else {
+            if (initNumber < endNumber) {
+                for (let i = initNumber-1; i <= endNumber; i++) {
+                    sum += i;
+                }
+            } else if (initNumber > endNumber) {
+                for (let i = initNumber; i >= endNumber; i--) {
+                    sum += i;
+                }
+            }
+            return sum
+        }
+    }
 
-}
 
 module.exports = sumAll
