368845b16c6bf0d619c0667b9a19820a4f83730b
Melanie Vanput <melanie.vanput@gmail.com>
2020-12-08 15:40:45 +0100

    resolve temperature conversion exercice

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(farenheit) {
+  let cel = Math.round((farenheit - 32) * 5 / 9 * 10) / 10 ;
+  return cel;
 }
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  let far = Math.round((celsius * 9 / 5 + 32) * 10) / 10 ;
+  return far;
 }
 
 module.exports = {
