2d0ec47f4b0083c6fec6d97ba2700e5fcb2d9808
Melanie Vanput <melanie.vanput@gmail.com>
2021-01-07 17:58:47 +0100

    resolve fibonacci exercice

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    let first = 0;
+    let second = 1;
+    let fibonacciTotal = 1;
+    if (index < 1) {
+        return "OOPS";
+    }
+    for (var i = 1; i < index; i++) {
+        fibonacciTotal = first + second;
+        first = second;
+        second = fibonacciTotal;
+    }
+    return fibonacciTotal
 }
 
 module.exports = fibonacci
