69de03032c9743d153386790d9d0700417c3a4e9
Melanie Vanput <melanie.vanput@gmail.com>
2021-01-07 17:58:09 +0100

    resolve calculator

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,37 @@
-function add () {
-	
+function add (a, b) {
+	return a + b
 }
 
-function subtract () {
-	
+function subtract (a, b) {
+	return a - b;
 }
 
-function sum () {
-	
+function sum (arr) {
+	let sum = 0;
+	for(let i = 0; i < arr.length; i++) {
+		sum += arr[i]
+	}
+	return sum
 }
 
-function multiply () {
-	
+function multiply (arr) {
+	let total = 1;
+	for(let i = 0; i < arr.length; i++) {
+	total *= arr[i]
+	}
+	return total
 }
 
-function power() {
-	
+function power(a, b) {
+	return Math.pow(a, b)
 }
 
-function factorial() {
-	
+function factorial(a) {
+	let total = 1
+	for(let i = a; i > 0; i--) {
+		total *= i
+	}
+	return total
 }
 
 module.exports = {
