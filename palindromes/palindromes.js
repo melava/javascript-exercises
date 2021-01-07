a5fd1c88648b08d84018d83ac6877aca14032f80
Melanie Vanput <melanie.vanput@gmail.com>
2021-01-07 18:00:17 +0100

    resolve palindrome exercice

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function() {
+const palindromes = function(string) {
+    string = string.toLowerCase().replace(/[^a-z]+/g,'').split('');
+    let arr = string.toString();
+    let pal = string.reverse().toString();
 
+    return arr === pal
 }
 
 module.exports = palindromes
