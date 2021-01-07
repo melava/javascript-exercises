290ba53ff742ae24d80f887913f1462914302210
Melanie Vanput <melanie.vanput@gmail.com>
2021-01-07 17:59:19 +0100

    resolve find the oldest

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-let findTheOldest = function() {
-
+let findTheOldest = function(people) {
+    people.forEach(person => {
+        if (person.yearOfDeath === undefined) {
+            let date = new Date();
+            let thisYear = date.getFullYear();
+            person.yearOfDeath = thisYear;
+        }
+    });
+    let oldest = people.sort((a,b) => {
+        last = a.yearOfDeath - a.yearOfBirth;
+        next = b.yearOfDeath - b.yearOfBirth;
+        return last > next ? -1 : 1;  
+    });
+    return oldest[0]
 }
 
 module.exports = findTheOldest
