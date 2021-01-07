410a384bbc2c85364a21979c2abc55c2a768a7d3
Melanie Vanput <melanie.vanput@gmail.com>
2021-01-07 17:59:47 +0100

    resolve get the titles

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,17 @@
 const getTheTitles = function() {
+    const books = [
+        {
+          title: 'Book',
+          author: 'Name'
+        },
+        {
+          title: 'Book2',
+          author: 'Name2'
+        }
+    ]
 
+    let bookTitles = books.map(book => book.title)
+    return bookTitles;
 }
 
 module.exports = getTheTitles;
